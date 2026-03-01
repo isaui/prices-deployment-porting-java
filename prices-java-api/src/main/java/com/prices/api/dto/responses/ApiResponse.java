bffacee96f1690e2dca19454e337c21908ea7d8c
isaui <isadestroyed17@gmail.com>
2026-03-01 05:54:07 +0000

    update api response

--- prices-java-api/src/main/java/com/prices/api/dto/responses/ApiResponse.java
+++ prices-java-api/src/main/java/com/prices/api/dto/responses/ApiResponse.java
@@ -19,4 +19,12 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> success(String message, T data) {
         return new ApiResponse<>(true, message, data);
     }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, null, data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
 }
